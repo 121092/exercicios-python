34f36122259a5dd09c14f0e260bdef5730d7bbc8
Vanessa Cardoso <vanessacardosofigueira@gmail.com>
2023-03-07 22:29:41 -0300

    meu primeiro comit

---
+++ advinhacao.py
@@ -0,0 +1,18 @@
+#print("Bem vindos ao jogo advinhação")
+
+numero_secreto = 40
+chute_str = input ("digite o numero: ")
+print("voce digitou", chute_str)
+
+chute_str = int(chute_str)
+
+if (numero_secreto == chute_str):
+    print("voce acertou")
+
+else:
+    if (chute_str > numero_secreto):
+        print("errou! seu numero é MAIOR que o numero secreto")
+    elif (chute_str < numero_secreto):
+        print("errou! seu numero é MENOR que o numero secreto")
+
+print("FIM DE JOGO")
---
+++ exercicio.py
@@ -0,0 +1,20 @@
+
+
+#print("Brasil ganhou 5 titulos mundiais")
+
+#print("Brasil", "ganhou", 5, "titulos", "mundiais", sep="-")
+
+#print("Brasil", "ganhou", 5, "titulos", "mundiais", end="!\n")
+
+#print("Brasil", "ganhou", 5, "titulos", "mundiais", end="")
+
+pais = "italia"
+#print(type(pais))
+quantidade = 4
+#print(type(quantidade))
+#print(pais, "ganhou", quantidade, "titulos mundiais")
+
+dia = 12 
+mes = 10
+ano = 1992
+print(dia, mes, ano, sep="/")
