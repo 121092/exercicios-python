f6974cf1116a1b2710da2aa25b4b9197b0481713
vanessa cardoso <vanessacardosofigueira@gmail.com>
2023-03-20 22:23:23 -0300

    classe documentada = com docstring

--- classe.py
+++ classe.py
@@ -1,16 +1,22 @@
 class Conta():
 
     def __init__(self, saldo, dono, cpf=None):
+        """
+            Metodo especial executado quando instancia uma classe.
+            instanciar uma classe e quando chamamos ela pelo seu nome e passamos todos os parametros obrigatorios descritos no __init__.
+            Instanciar: ocorre quando voce chama uma classe pelo seu nome, onde todos os parametros serao passados (parametros do __init__).
+            Ex: Linhas 30 e 31.
+        """
         self.saldo = saldo
         self.dono = dono
         self.cpf = cpf
-        
-    
+
+
     def enviar_dinheiro(self, conta_destino, valor):
         self.saldo = self.saldo - valor
-        conta_destino.saldo = conta_destino.saldo + valor 
-        
-    
+        conta_destino.saldo = conta_destino.saldo + valor
+
+
     def cadastrar_cpf(self, numero_cpf):
         if not self.cpf == None:
             pass
@@ -18,7 +24,7 @@ class Conta():
         else:
             self.cpf = numero_cpf
 
-    
+
 
 
 usuario = Conta(saldo=10, dono="lis", cpf="01286566541")
